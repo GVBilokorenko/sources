d8639002414ee516b6c47a64103adff01a0ba0ee
GVBilokorenko <abilokorenko@gmail.com>
2021-12-03 12:33:33 +0200

    connect swagger/ add openapi.yaml)

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swStats = require("swagger-stats");
+
+const swaggerDocument = YAML.load("./openapi.yaml");
 
 const catalog = require("./catalog.js");
 // catalog - database emulation
 
 const server = express();
+
+server.use(swStats.getMiddleware({ swaggerSpec: swaggerDocument }));
+server.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use(cors());
 server.use(express.json());
 
